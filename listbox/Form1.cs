855ed06a5542f4a3c29221de60e8d7d1f4a4fa50
Tania <ladytatiana100@gmail.com>
2018-11-28 10:26:19 +0200

    Добавьте файлы проекта.

---
+++ listbox/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedItem = null;
+            label2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                if (listBox1.SelectedIndex == 6)
+                    label2.Text = "Ура! Сьогодні у нас  " + listBox1.SelectedItem;
+                else
+                    label2.Text = "Сьогодні у нас  " + listBox1.SelectedItem;
+            }
+            else
+                MessageBox.Show("Елемент не вибраний із списку", "Помилка");
+
+        }
+    }
+}
